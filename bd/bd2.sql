3e7bcbcf914e8caaafd6dc11afe10c95dd135c1b
Gregorio Garcia Choung <cibertron12345@gmail.com>
2019-03-25 12:22:28 -0500

    consulta auto disponible bd2 sp_lista

---
+++ bd/bd2.sql
@@ -0,0 +1,335 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-03-2019 a las 18:15:29
+-- Versión del servidor: 5.6.26
+-- Versión de PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd2`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_consultaAutomovilDisponible`(ini date, fin date)
+begin
+select * from automovils where matricula not in(select idAuto from reservas r where 
+ini between fechaIni and fechaFin
+or
+fin between fechaIni and fechaFin
+or 
+fechaIni >= ini and fechaFin <=fin
+and
+r.estado = '1');
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_lista`(f1 date, f2 date)
+begin
+-- > 7 10
+-- < 5 8
+-- > < 7 8
+-- < > 5 10
+-- = = 6 9
+-- = > 6 10
+-- < = 5 10
+-- = < 6 8
+-- > = 7 9
+-- 
+-- 5 6, 9 10
+
+select * from reservas 
+where f1 between fechaIni and fechaFin 
+or f2 between fechaIni and fechaFin
+or fechaIni >= f1 and fechaFin <=f2;
+
+select * from reservas
+where f1 >= fechaIni and f1 <= fechaFin
+or f2 >= fechaIni and f2 <=FechaFin
+or fechaIni >= f1 and fechaFin <= f2;
+
+select * from reservas
+where fechaIni between f1 and f2 
+or fechaFin between f1 and f2
+or fechaIni >= f1 and fechaFin <=f2
+or fechaIni <= f1 and fechaFin >=f2;
+
+end$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `automovils`
+--
+
+CREATE TABLE IF NOT EXISTS `automovils` (
+  `matricula` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `modelo` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `anno` int(11) NOT NULL,
+  `precio` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `automovils`
+--
+
+INSERT INTO `automovils` (`matricula`, `modelo`, `anno`, `precio`) VALUES
+('ABC-931', 'sony 3', 2017, 300),
+('ABC-A32', 'pun racer', 2007, 400),
+('CC1-2AA', 'orion 45', 2001, 100);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `licencia` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`licencia`, `nombre`, `email`, `telefono`, `pass`) VALUES
+('G71962605', '', '', '', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int(10) unsigned NOT NULL,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_03_23_195521_create_notas_table', 1),
+(4, '2019_03_23_213039_create_clientes_table', 1),
+(5, '2019_03_23_213304_create_automovils_table', 1),
+(6, '2019_03_23_213416_create_reservas_table', 1),
+(7, '2019_03_23_213556_create_recepcionistas_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE IF NOT EXISTS `notas` (
+  `id` int(10) unsigned NOT NULL,
+  `nombre` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recepcionistas`
+--
+
+CREATE TABLE IF NOT EXISTS `recepcionistas` (
+  `dni` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nombre` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recepcion_reservas`
+--
+
+CREATE TABLE IF NOT EXISTS `recepcion_reservas` (
+  `id` int(10) unsigned NOT NULL,
+  `idReserva` int(11) NOT NULL,
+  `idRecepcionista` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `estado` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE IF NOT EXISTS `reservas` (
+  `id` int(10) unsigned NOT NULL,
+  `idCliente` varchar(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `idAuto` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fechaIni` date NOT NULL,
+  `fechaFin` date NOT NULL,
+  `estado` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `reservas`
+--
+
+INSERT INTO `reservas` (`id`, `idCliente`, `idAuto`, `fechaIni`, `fechaFin`, `estado`) VALUES
+(1, 'G71962605', 'ABC-931', '2018-05-30', '2018-09-02', '1'),
+(2, 'G71962605', 'ABC-931', '2017-01-01', '2017-02-02', '1'),
+(3, 'G71962605', 'ABC-931', '2019-01-06', '2019-01-09', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `automovils`
+--
+ALTER TABLE `automovils`
+  ADD PRIMARY KEY (`matricula`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`licencia`);
+
+--
+-- Indices de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indices de la tabla `recepcionistas`
+--
+ALTER TABLE `recepcionistas`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `recepcion_reservas`
+--
+ALTER TABLE `recepcion_reservas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `reservas_idcliente_foreign` (`idCliente`),
+  ADD KEY `reservas_idauto_foreign` (`idAuto`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `recepcion_reservas`
+--
+ALTER TABLE `recepcion_reservas`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `reservas`
+--
+ALTER TABLE `reservas`
+  ADD CONSTRAINT `reservas_idauto_foreign` FOREIGN KEY (`idAuto`) REFERENCES `automovils` (`matricula`),
+  ADD CONSTRAINT `reservas_idcliente_foreign` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`licencia`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
